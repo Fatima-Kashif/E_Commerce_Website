bb7f1413d48cbbda9c412f0ae8d3e8650d723af4
Fatima-Kashif <kashiffatima465@gmail.com>
2025-03-19 20:07:15 +0500

    change

--- studio-ecommerce-website/sanity.config.js
+++ studio-ecommerce-website/sanity.config.js
@@ -3,17 +3,17 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
+
 export default defineConfig({
   name: 'default',
   title: 'Ecommerce Website',
+  basePath:'/studio',
 
   projectId: 'qx635353',
   dataset: 'production',
 
   plugins: [structureTool(), visionTool()],
-  preview: {
-    url: 'https://e-commerce-website-three-vert.vercel.app/', 
-  },
+ 
 
   schema: {
     types: schemaTypes,
