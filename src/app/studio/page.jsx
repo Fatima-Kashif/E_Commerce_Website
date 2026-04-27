c31fdcc4694bc97f93c47a6267ba432bc32b45a8
Fatima-Kashif <kashiffatima465@gmail.com>
2026-04-27 17:13:55 +0500

    fix studio page and missing dependencies

--- src/app/studio/page.jsx
+++ src/app/studio/page.jsx
@@ -1,5 +1,6 @@
-export const dynamic = 'force-static'
 'use client';
+export const dynamic = 'force-static'
+
 import { NextStudio } from 'next-sanity/studio';
 import config from '../../../studio-ecommerce-website/sanity.config';  
 
